e2939ccf6376233c4a5d7a2580fd95c01a58dc3e
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-19 15:13:58 +0530

    fixes

--- ansible/files/docker-install.sh
+++ ansible/files/docker-install.sh
@@ -11,11 +11,11 @@ sudo apt-get install -y \
     lsb-release
 
 # Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Adding to the key repo list
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+ echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Updating packages and Installing docker
