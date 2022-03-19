a188c61d47c85fe0385d7430b4bda2f3493f8f49
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-19 14:53:11 +0530

    Added docker file and script

---
+++ ansible/files/docker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Updating packages
+sudo apt-get update
+
+# Adding required packages
+sudo apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Adding to the key repo list
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Updating packages and Installing docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
