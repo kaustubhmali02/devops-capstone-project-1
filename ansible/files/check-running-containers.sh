f02a8751b0c3da860d48939af368b97bea1371e7
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-23 13:10:48 +0530

    fixes

--- ansible/files/check-running-containers.sh
+++ ansible/files/check-running-containers.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 if [ "$(docker ps -q -f name=website)" ];then
     echo "Running Countainers. Removing them"
-    docker rm -f website
+    docker rm -f $(docker ps -q -f name=website)
+    if [ "$(docker ps -aq -f status=exited -f name=website)" ]; then
+        # cleanup
+        docker rm -f $(docker ps -aq -f status=exited -f name=website)
+    fi
 else
     echo "Din't find any Running Containers"
 fi
