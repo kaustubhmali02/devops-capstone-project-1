37ed4596ffd1cf948b84d62ecd79abc077f000b1
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-23 20:10:44 +0530

    fixes

--- ansible/files/create-volume.sh
+++ ansible/files/create-volume.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # Creating a volume only if needed
-if [ "$(docker volume ls -q -f name=get-output )" ]; then
+if [ "$(docker volume ls -q -f name=get-output)" ]; then
     echo "Volume: get-output present. Not creating a new one."
 else 
     echo "Creating a volume for retriving the website-selenium-test output and jar file"
-    docker volume create get-output
+    docker volume create \
+    --name get-output
+    --opt type=none
+    --opt device=/home/jenkins/output
+    --opt o=bind
 fi
 
