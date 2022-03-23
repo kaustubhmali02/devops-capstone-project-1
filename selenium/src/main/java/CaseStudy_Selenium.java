d7271a793797c10ee53fb6a2abc92130b10e12d7
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-23 16:47:36 +0530

    fixes

--- selenium/src/main/java/CaseStudy_Selenium.java
+++ selenium/src/main/java/CaseStudy_Selenium.java
@@ -25,7 +25,7 @@ public class CaseStudy_Selenium {
 
     @BeforeTest
     private void initChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         driver = new ChromeDriver(options);
