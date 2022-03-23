980a92b3ff0cde5e95e755c3a2f6424cc622ad9e
kaustubhmali02 <kaustubhmali02@gmail.com>
2022-03-23 20:26:49 +0530

    fixes

---
+++ ansible/files/create-volumes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Creating a volume only if needed
+if [ "$(docker volume ls -q -f name=get-output)" ]; then
+    echo "Volume: get-output present. Not creating a new one."
+else 
+    echo "Creating a volume for retriving the jar files"
+    docker volume create \
+    --name get-target
+    --opt type=none
+    --opt device=/home/jenkins/output/java
+    --opt o=bind
+fi
+
+if [ "$(docker volume ls -q -f name=get-test-output)" ]; then
+    echo "Volume: get-test-output present. Not creating a new one."
+else
+    echo "Creating a volume for retriving the test-result files"
+    docker volume create \
+    --name get-target
+    --opt type=none
+    --opt device=/home/jenkins/output/test-results
+    --opt o=bind
+fi
